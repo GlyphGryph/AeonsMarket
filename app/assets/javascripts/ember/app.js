adba6bb0d26b2f5d26d0f6e4da07cd37c2faa7b1
GlyphGryph <glyphgryph@gmail.com>
2013-04-22 22:00:25 -0400

    Emeber elements included, configured, and working correctly. Additional Rails components configured.

--- app/assets/javascripts/ember/app.js
+++ app/assets/javascripts/ember/app.js
@@ -3,29 +3,5 @@
 
 window.App = Ember.Application.create();
 
-App.Router.map(function() {
-  this.resource('album', { path: '/album/:album_id' });
-});
-
-App.ApplicationRoute = Ember.Route.extend({
-});
-
-App.IndexRoute = Ember.Route.extend({
-  model: function(){
-    return App.ALBUM_FIXTURES;
-  }
-});
-
-App.AlbumRoute = Ember.Route.extend({
-  model: function(params){
-    return App.ALBUM_FIXTURES.findProperty('id', params.album_id);
-  }
-});
-
-Ember.Handlebars.helper('format-duration', function(duration) {
-  var minutes = duration%60;
-  if(minutes < 10){ minutes = "0"+minutes; }
-  return "%@:%@".fmt(Math.floor(duration/60), minutes );
-});
-
 })();
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aeonsmarket::Application.config.secret_token = '6dd2f7ad9907f17a93c4a903634b7ebb5fbdcbf710707fbe7108075494f5ad30177f92065d1dc91c065f1f7951737d98138789e60f8a7de421443c76556eb53f'
